34c29c1b913fb1071c648f41bd70409ed0a73047
robert trebor <treborx555@gmail.com>
2020-08-06 20:26:23 +0200

    should work now

--- src/main/java/com/robertx22/craftable_utilities/main/CommonInit.java
+++ src/main/java/com/robertx22/craftable_utilities/main/CommonInit.java
@@ -1,13 +1,16 @@
 package com.robertx22.craftable_utilities.main;
 
 import com.robertx22.library_of_exile.events.base.EventConsumer;
+import com.robertx22.library_of_exile.events.base.ExileEventCaller;
 import com.robertx22.library_of_exile.events.base.ExileEvents;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 
 public class CommonInit implements ModInitializer {
+
+    public static ExileEventCaller<ExileEvents.OnMobDeath> PLAYER_DEATH_EVENT = new ExileEventCaller();
 
     @Override
     public void onInitialize() {
@@ -16,10 +19,10 @@ public class CommonInit implements ModInitializer {
 
         AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
 
-        ExileEvents.MOB_DEATH.register(new EventConsumer<ExileEvents.OnMobDeath>() {
+        CommonInit.PLAYER_DEATH_EVENT.register(new EventConsumer<ExileEvents.OnMobDeath>() {
             @Override
             public void accept(ExileEvents.OnMobDeath event) {
-                if (event.mob instanceof PlayerEntity) {
+                if (event.mob instanceof ServerPlayerEntity) {
                     try {
                         Components.INSTANCE.ENTITY_DATA.get(event.mob).pos = event.mob.getBlockPos();
                         Components.INSTANCE.ENTITY_DATA.get(event.mob).dim = event.mob.world.getDimensionRegistryKey()
